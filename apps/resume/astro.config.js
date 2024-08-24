ab7ffe696d7963f939f097af4c307f4818746aee
noam <noamkadosh91@gmail.com>
2024-08-23 23:56:44 -0700

    add node adpter to astro

--- apps/resume/astro.config.js
+++ apps/resume/astro.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
 import mdx from "@astrojs/mdx"
-
+import node from "@astrojs/node"
 const { CI } = process.env
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: node({
+        mode: "standalone",
+    }),
     devToolbar: {
         enabled: !CI,
     },
