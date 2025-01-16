3a245d7a68dec43eaa4f9dc85b71a55c4be37cf4
noam <noamkadosh91@gmail.com>
2025-01-15 22:15:12 -0800

    feat: adjust tsconfig paths

--- packages/eslint-config/core.js
+++ packages/eslint-config/core.js
@@ -31,7 +31,7 @@ const importOrder = [
         position: "before",
       },
       {
-        pattern: "@/**",
+        pattern: "@*/**",
         group: "internal",
         position: "after",
       },
