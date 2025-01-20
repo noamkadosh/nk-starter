4faaac5919b550d7efe9491d31032070a83a67dc
noam <noamkadosh91@gmail.com>
2025-01-20 10:44:05 -0800

    feat: update to node 22

--- apps/cms/next.config.ts
+++ apps/cms/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
+  // eslint-disable-next-line @typescript-eslint/require-await
   async redirects() {
     return [
       {
